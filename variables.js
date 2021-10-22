87c34463eee925a4a58c32547488669c3322828c
Dylan Duke <laytonmaes@gmail.com>
2021-10-22 14:28:11 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,14 @@
+var famousPerson = "Natalie Portman"
+var isActive = true
+famousPerson = "Emma Watson"
+
+var num3 = 23
+var num9 = 42
+var sum = num3+num9
+
+var animals = ["Komodo Dragon", "Pengolin", "Armadillo"]
+console.log(animals)
+animals.shift ()
+console.log(animals)
+
+console.log("The famous person has "+famousPerson.length+" characters in their name")
