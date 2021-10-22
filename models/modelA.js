9c5cb3446c03df321e26011540c43926b00f6eda
Dylan Duke <laytonmaes@gmail.com>
2021-10-22 14:49:55 -0600

    Add modelA and modelB and give modelA keys

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var airport = {
+  location1: "Runway",
+  location2: "Baggage claim",
+  planeChicagoArrived: true,
+  planeDallasDeparted: false,
+  numberOfPlanes: 32,
+  numberOfPassengers: 2343,
+  typesOfPlane: ["big", "small", "private"],
+  passengerClass: ["first", "economy", "business"],
+}
