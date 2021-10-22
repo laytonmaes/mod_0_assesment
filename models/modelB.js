8c596f5755c888525dbd5742dc798beff4ddb1c0
Dylan Duke <laytonmaes@gmail.com>
2021-10-22 14:58:01 -0600

    Add keys and object to modelB

---
+++ models/modelB.js
@@ -0,0 +1,11 @@
+var vehicle = {
+  carModel: ["Civic", "Tacoma", "Panzer"],
+  carAmounts: [8,13,142],
+  militaryMight: "Absolute",
+  civicFunction: "Ammunition",
+  enemiesDoomed: true,
+  alliesDoomed: true,
+  numberOfAllies:0,
+  numberOfEnemies:0,
+  messageDelivered: "All shall perish before our might, friend or foe",
+}
